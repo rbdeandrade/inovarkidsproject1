96be02cf5e8b8c5721656661f1d3b4e7db736e40
Renato Braga de Andrade <rbdeandrade@gmail.com>
2022-06-02 09:52:15 +0100

    add button description

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ window.onload = function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
-        alert("Complete o primeiro passo");
+        alert("Aponte o telemóvel para o Target);
       });
   };
